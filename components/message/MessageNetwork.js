832ff66cfc66b9072f63d0e41c3a129fabce185e
abbul <abbulrodriguez@hotmail.com>
2019-11-10 23:59:15 -0300

    error en ruta meesage/create

--- components/message/MessageNetwork.js
+++ components/message/MessageNetwork.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const response = require(`../../network/response`);
 const messageController = require(`./MessageController`);
 
-router.get(`/add`,(req,res)=>{
+router.post(`/create`,(req,res)=>{
 
     messageController.add(req.body.user,req.body.message).then(resolve=>{
         response.success(req,res,resolve);
@@ -13,12 +13,20 @@ router.get(`/add`,(req,res)=>{
 
 });
 
-router.get(`/get`,(req,res)=>{
+router.get(`/read`,(req,res)=>{
     messageController.list().then(resolve =>{
         response.success(req,res,resolve);
     }).catch(reject =>{
         response.error(req,res,reject);
     })
+});
+
+router.patch(`/update`,(req,res)=>{
+    if (req.query.error == "ok") {
+        response.error(req,res,"Error simulado",401,"serian los detalles del error");
+    }else{
+        response.success(req,res,"creado correctamente");
+    }
 });
 
 router.delete(`/delete`,(req,res)=>{
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser')
-const db = require(`./db`);
+const db = require(`./config/db`);
 const router = require(`./network/routes`);
 
 const app = express();
